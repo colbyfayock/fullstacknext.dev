ebd842fe6a3ef3a217b170a4a05184efc0254b6a
Colby Fayock <fayock@gmail.com>
2024-10-01 23:13:21 -0400

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 import Footer from "@/components/Footer";
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
